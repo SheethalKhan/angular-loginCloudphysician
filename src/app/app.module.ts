36b6c999297338765b8e3b24395ad79c0405f5cf
SheethalKhan <sheethalkhans06@gmail.com>
2022-01-27 23:09:44 +0530

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+const routes:Routes=[
+{path:'',redirectTo:'login',pathMatch:'full'},
+{path:'login',component:LoginComponent}
 
+]
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes),ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
